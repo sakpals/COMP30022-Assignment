2e60fbf222537b100334884cd2d96e4889de4219
bryce1234 <mathesonb@student.unimelb.edu.au>
2017-09-11 03:29:03 +1000

    Added the barebones design of the app that can be now build upon

--- app/src/main/java/net/noconroy/itproject/application/MainActivity.java
+++ app/src/main/java/net/noconroy/itproject/application/MainActivity.java
@@ -1,7 +1,9 @@
 package net.noconroy.itproject.application;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void camera(View view){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    public void addFriend(View view){
+        Intent intent = new Intent(this, AddFriendActivity.class);
+        startActivity(intent);
     }
 }
