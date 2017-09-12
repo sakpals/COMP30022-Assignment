e77a4e50c3a84f174369bcb99b27b0dd9575ad0e
bryce1234 <mathesonb@student.unimelb.edu.au>
2017-09-13 01:18:01 +1000

    A few of the tests like LoginTest were using username: test_username, even if that username hadn't been registered, so some unit tests were failing no matter what. To remedy this, I clumped registering and login together, so for example LoginTest, you would have to register INSIDE that test before you perform the login - so obviously a lot of this depends of Register working

--- app/src/test/java/net/noconroy/itproject/application/NetworkHelperUnitTest.java
+++ app/src/test/java/net/noconroy/itproject/application/NetworkHelperUnitTest.java
@@ -4,35 +4,55 @@ import org.junit.Test;
 
 import java.util.UUID;
 
+import static net.noconroy.itproject.application.NetworkHelper.Register;
+
 public class NetworkHelperUnitTest {
+
+    // TODO: Check tht the username is valid - check if white space works
 
     @Test
     public void RegisterTest() throws Exception {
         String username = UUID.randomUUID().toString();
-        if (!NetworkHelper.Register(username, "test_password", "test_avatar_url", "test_description").equals("201"))
+        if (!Register(username, "test_password", "test_avatar_url", "test_description").equals("201"))
             throw new AssertionError();
     }
 
+    // This test assume RegisterTest is functioning correctly
     @Test
     public void LoginTest() throws Exception {
-        String access_token = NetworkHelper.Login("test_username", "test_password");
+        final String Error500 = "This error tends to usually mean you're try" +
+                "to login with a username that doesn't exist in this in this" +
+                "context ";
+
+        String username = UUID.randomUUID().toString();
+        NetworkHelper.Register(username, "test_password", "test_avatar_url", "test_description");
+
+        String access_token = NetworkHelper.Login(username, "test_password");
         if (access_token.equals("401")) throw new AssertionError();
+        if (access_token.equals("500")) throw new AssertionError(Error500);
     }
 
-    // Tests below here assume Login works
+    // Tests below here assume Register and Login works
     // TODO: Something wrong with my handling of access_token
     @Test
     public void LogoutTest() throws Exception {
-        // Log in first
-        String access_token = NetworkHelper.Login("test_username", "test_password");
+        // Register and login first
+        String username = UUID.randomUUID().toString();
+        NetworkHelper.Register(username, "test_password", "test_avatar_url", "test_description");
+        String access_token = NetworkHelper.Login(username, "test_password");
+
         if (!NetworkHelper.Logout(access_token).equals("200")) throw new AssertionError();
     }
 
     @Test
     public void UpdateProfileTest() throws Exception {
-        // Log in first
-        String access_token = NetworkHelper.Login("test_username", "test_password");
-        if (!NetworkHelper.UpdateProfile("test_username", "test_password", "test_avatar_url", "test_description", access_token).equals("204"))
+        // Register and Login first
+        String username = UUID.randomUUID().toString();
+        NetworkHelper.Register(username, "test_password", "test_avatar_url", "test_description");
+        String access_token = NetworkHelper.Login(username, "test_password");
+
+        if (!NetworkHelper.UpdateProfile(username, "test_password", "test_avatar_url", "test_description", access_token).equals("204"))
             throw new AssertionError();
     }
+
 }
