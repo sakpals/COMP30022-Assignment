5dc97dfec3a5bff54afb2d8ac8b6f8b882663295
bryce1234 <mathesonb@student.unimelb.edu.au>
2017-09-20 00:14:44 +1000

    Added registering to UI

--- app/src/main/java/net/noconroy/itproject/application/MainActivity.java
+++ app/src/main/java/net/noconroy/itproject/application/MainActivity.java
@@ -4,13 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String access_token = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String access_token = intent.getStringExtra(RegisterActivity.ACCESS_TOKEN_MESSAGE);
+
+        if (access_token != null){
+
+            // Hide register button
+            Button registerButton = (Button) findViewById(R.id.Registerbutton);
+            registerButton.setVisibility(View.GONE);
+
+            // Change text of Login button
+            Button loginButton = (Button) findViewById(R.id.LoginButton);
+            loginButton.setText("Logged in");
+
+            // Disable logging in, as you're alraedy logged
+            loginButton.setEnabled(false);
+        }
+    }
+
+    public void register(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 
     public void login(View view){
--- app/src/main/java/net/noconroy/itproject/application/RegisterActivity.java
+++ app/src/main/java/net/noconroy/itproject/application/RegisterActivity.java
@@ -1,13 +1,173 @@
 package net.noconroy.itproject.application;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public static final String ACCESS_TOKEN_MESSAGE = "aser_access_token";
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private RegisterActivity.UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private EditText mUsernameView;
+    private EditText mPasswordView;
+    private Button mLoginButton;
+    private View mProgressView;
+    private View mLoginFormView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Set up the login form.
+        mUsernameView = (EditText) findViewById(R.id.usernameField);
+        mPasswordView = (EditText) findViewById(R.id.passwordField);
+
+        mLoginButton = (Button)findViewById(R.id.attemptRegisterButton);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                attemptRegister();
+            }
+        });
+
+    }
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptRegister() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            //showProgress(true);
+            mAuthTask = new RegisterActivity.UserLoginTask(username, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mUsername;
+        private final String mPassword;
+        private String serverResponse;
+
+        UserLoginTask(String username, String password) {
+            mUsername = username;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            //serverResponse = NetworkHelper.Register(mUsername, mPassword, "", "No Description");
+
+            // Server returns http message 201 if it is successful
+            //if (serverResponse.equals("201")) return true;
+
+            // TODO: change this to false
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            //showProgress(false);
+
+            if (success) {
+
+                // Create Toast popup
+                Context context = getApplicationContext();
+                CharSequence text = "Register Successful!!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                // Send an intent to main activity with the user's access token
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+
+                intent.putExtra(ACCESS_TOKEN_MESSAGE, "insert-token-here");
+                startActivity(intent);
+
+                // Go back to main activity
+                //finish();
+            } else {
+                if (serverResponse.equals("500")) mPasswordView.setError("Username taken, try again");
+                else mPasswordView.setError("Server Error");
+                mUsernameView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            //showProgress(false);
+        }
     }
 }
+
+
+
+
+
