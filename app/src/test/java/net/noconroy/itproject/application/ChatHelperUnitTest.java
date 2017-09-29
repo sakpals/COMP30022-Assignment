180e57d761ccbe7b8c1363019d78499f5c6f6b7d
bryce1234 <mathesonb@student.unimelb.edu.au>
2017-09-29 13:18:21 +1000

    Changed throwing exceptions to assert, and put hardcoded paramaters as constants

--- app/src/test/java/net/noconroy/itproject/application/ChatHelperUnitTest.java
+++ app/src/test/java/net/noconroy/itproject/application/ChatHelperUnitTest.java
@@ -1,13 +1,11 @@
 package net.noconroy.itproject.application;
 
-import com.google.gson.Gson;
-
-import org.junit.AssumptionViolatedException;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 public class ChatHelperUnitTest {
 
@@ -18,37 +16,44 @@ public class ChatHelperUnitTest {
     private static String access_token;
     private static String access_token2;
 
+    // Arbitary variables used to test that methods are working correctly
+    private final String test_password = "test_password";
+    private final String test_avatar_url = "test_avatar_url";
+    private final String test_description = "test_description";
+    private final String test_message = "test_message";
+
+
     @Before
     public void AccountSetup() {
         username = UUID.randomUUID().toString();
         username2 = UUID.randomUUID().toString();
-        NetworkHelper.Register(username, "test_password", "test_avatar_url", "test_description");
-        NetworkHelper.Register(username2, "test_password", "test_avatar_url", "test_description");
-        access_token = NetworkHelper.Login(username, "test_password");
-        access_token2 = NetworkHelper.Login(username2, "test_password");
+        NetworkHelper.Register(username, test_password, test_avatar_url, test_description);
+        NetworkHelper.Register(username2, test_password, test_avatar_url, test_description);
+        access_token = NetworkHelper.Login(username, test_password);
+        access_token2 = NetworkHelper.Login(username2, test_password);
     }
 
     @Test
     public void CreateChannelTest() {
         String channel_name = UUID.randomUUID().toString();
-        if (!ChatHelper.CreateChannel(channel_name, access_token, true).equals("201"))
-            throw new AssertionError();
+        String response = ChatHelper.CreateChannel(channel_name, access_token, true);
+        assertEquals(response, "201");
     }
 
     @Test
     public void DeleteChannelTest() {
         String channel_name = UUID.randomUUID().toString();
         ChatHelper.CreateChannel(channel_name, access_token, true);
-        if (!ChatHelper.DeleteChannel(channel_name, access_token).equals("200"))
-            throw new AssertionError();
+        String response = ChatHelper.DeleteChannel(channel_name, access_token);
+        assertEquals(response, "200");
     }
 
     @Test
     public void SubscribeChannelTest() {
         String channel_name = UUID.randomUUID().toString();
         ChatHelper.CreateChannel(channel_name, access_token, true);
-        if (!ChatHelper.SubscribeChannel(channel_name, access_token2).equals("200"))
-            throw new AssertionError();
+        String response = ChatHelper.SubscribeChannel(channel_name, access_token2);
+        assertEquals(response, "200");
     }
 
     @Test
@@ -56,8 +61,8 @@ public class ChatHelperUnitTest {
         String channel_name = UUID.randomUUID().toString();
         ChatHelper.CreateChannel(channel_name, access_token, true);
         ChatHelper.SubscribeChannel(channel_name, access_token2);
-        if (!ChatHelper.LeaveChannel(channel_name, access_token2).equals("200"))
-            throw new AssertionError();
+        String response = ChatHelper.LeaveChannel(channel_name, access_token2);
+        assertEquals(response, "200");
     }
 
     @Test
@@ -65,8 +70,8 @@ public class ChatHelperUnitTest {
         String channel_name = UUID.randomUUID().toString();
         ChatHelper.CreateChannel(channel_name, access_token, true);
         ChatHelper.SubscribeChannel(channel_name, access_token2);
-        if (!ChatHelper.MessageChannel(channel_name, "test_message", access_token2).equals("200"))
-            throw new AssertionError();
+        String response = ChatHelper.MessageChannel(channel_name, test_message, access_token2);
+        assertEquals(response, "200");
     }
 
     @Test
