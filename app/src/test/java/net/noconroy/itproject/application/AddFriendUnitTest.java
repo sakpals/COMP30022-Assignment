acdc0b91c35b748ad3780ba6cf0c2b7761cfc872
sakpals <sakpals@student.unimelb.edu.au>
2017-10-07 12:31:24 +1100

    added more tests

--- app/src/test/java/net/noconroy/itproject/application/AddFriendUnitTest.java
+++ app/src/test/java/net/noconroy/itproject/application/AddFriendUnitTest.java
@@ -1,27 +1,18 @@
 package net.noconroy.itproject.application;
 
-import android.text.Editable;
-import android.widget.EditText;
 
+import android.widget.EditText;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
 import java.util.UUID;
-import android.text.Editable;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import net.noconroy.itproject.application.AddFriendActivity.AddUserAsFriendTask;
 
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyMapOf;
-import static org.mockito.Matchers.eq;
 
 /**
  * Created by sampadasakpal on 24/9/17.
@@ -31,6 +22,7 @@ import static org.mockito.Matchers.eq;
 @PrepareForTest({NetworkHelper.class, AddUserAsFriendTask.class})
 public class AddFriendUnitTest {
 
+
     private static String username1;
     private static String username2;
     private static String access_token1 = "r24kj32fkjelwjrklwjerlkewjrlwejrlwjeklrjwe";
@@ -39,13 +31,11 @@ public class AddFriendUnitTest {
     private static String password2 = "password2_test";
 
     private String response;
-
     private AddFriendActivity addFriendActivity = Mockito.spy(new AddFriendActivity());
-
     private EditText findUser = Mockito.mock(EditText.class);
-
     private AddUserAsFriendTask user1_task = null;
 
+    
     @Before
     public void setUp() {
 
@@ -62,8 +52,8 @@ public class AddFriendUnitTest {
         Mockito.when(NetworkHelper.Register(username2, password2, "", "")).thenReturn("201");
         Mockito.when(NetworkHelper.Login(username2, password2)).thenReturn(access_token2);
 
-        Mockito.when(NetworkHelper.AddFriend(username2, access_token1)).thenReturn("201");
-        Mockito.when(NetworkHelper.AddFriend(username1, access_token2)).thenReturn("201");
+        Mockito.when(NetworkHelper.AddFriend(username2, access_token1)).thenReturn("201", "401", "401");
+        Mockito.when(NetworkHelper.AddFriend(username1, access_token2)).thenReturn("201", "401", "401");
 
         try {
             PowerMockito.whenNew(AddUserAsFriendTask.class).withArguments(username2, access_token1).thenReturn(user1_task);
@@ -97,6 +87,35 @@ public class AddFriendUnitTest {
         assertTrue(response2.equals("201"));
 
     }
+
+    /* tests to see whether friend request fails when trying to add the same friend twice */
+    @Test
+    public void testAddFriendTwice() {
+        String response = NetworkHelper.AddFriend(username1, access_token2);
+        assertTrue(response.equals("201"));
+
+        String response2 = NetworkHelper.AddFriend(username1, access_token2);
+        assertTrue(response2.equals("401"));
+    }
+
+    /* tests to see whether friend request fails due to some network error */
+    @Test
+    public void testNetworkError() {
+        Mockito.when(NetworkHelper.AddFriend(username1, access_token2)).thenReturn("500");
+        String response = NetworkHelper.AddFriend(username1, access_token2);
+        assertTrue(response.equals("500"));
+
+    }
+
+    /* tests to see whether friend request fails due to user (being searched for) not existing  */
+    @Test
+    public void testFriendSearchError() {
+        Mockito.when(NetworkHelper.AddFriend(username1, access_token2)).thenReturn("404");
+        String response = NetworkHelper.AddFriend(username1, access_token2);
+        assertTrue(response.equals("404"));
+
+    }
+
 
 
 }
