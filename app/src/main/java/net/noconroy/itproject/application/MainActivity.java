6d996232ccedc407f69b8dd3613a74b9ba96f31f
Mattias <matwil_3@hotmail.com>
2017-10-07 16:11:41 +1100

    fix small error

--- app/src/main/java/net/noconroy/itproject/application/MainActivity.java
+++ app/src/main/java/net/noconroy/itproject/application/MainActivity.java
@@ -161,6 +161,7 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtras(userClickedOn);
 
         startActivity(intent);
+    }
 
 
     public void doBindLocationService() {
